0a4a41b90f91c031560c7186e1ad0664fe426820
Ramunas Siupinys <ramsiu@icloud.com>
2025-02-27 18:04:48 +0000

    Method Assignment

---
+++ Basic_C#_Programs/C# .NETFrameP2/Method Assignment/MathOperation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class MathOperation
+    {
+        // Create a method that takes two integers as parameters
+        // The second parameter is optional, with a default value of 5
+        public int PerformOperation(int num1, int num2 = 5)
+        {
+            // Perform addition operation and return the result
+            return num1 + num2;
+        }
+    }
+}
---
+++ Basic_C#_Programs/C# .NETFrameP2/Method Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate MathOperation class
+            MathOperation mathOp = new MathOperation();
+
+            // Ask the user to input the first number
+            Console.WriteLine("Enter an integer: ");
+             int number1 = Convert.ToInt32(Console.ReadLine());
+
+            // Ask the user for a second number (optional)
+            Console.WriteLine("Enter a second integer (or press Enter to use default value): ");
+            string input = Console.ReadLine();
+
+            // Check if the user entered a second number
+            if (int.TryParse(input, out int number2))
+            {
+                // Call the method with two parameters
+                int result = mathOp.PerformOperation(number1, number2);
+                Console.WriteLine(number1 + " + " + number2 + " = " + result);
+            }
+            else
+            {
+                // Call the method with only one parameter (default value is used)
+                int result = mathOp.PerformOperation(number1);
+                Console.WriteLine(number1 + " + Default (5) = " + result);
+            }
+
+            // Keep the console open
+            Console.ReadLine();
+        }
+    }
+}
