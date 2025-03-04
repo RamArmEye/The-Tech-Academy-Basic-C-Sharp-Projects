9b8761911af11a7123974a5e433771690ad4a282
Ramunas Siupinys <ramsiu@icloud.com>
2025-03-04 21:15:41 +0000

    Tutorial - Operator Overloading
    Tutorial - Operator Overloading----COMPLETED

--- Basic_C#_Programs/C# .NETFrameP2/Twenty One/Program.cs
+++ Basic_C#_Programs/C# .NETFrameP2/Twenty One/Program.cs
@@ -10,37 +10,24 @@ namespace Twenty_One
     {
         static void Main(string[] args)
         {
-            //List<Game> games = new List<Game>();    
-            //Game GAME = new TwentyOneGame();
-            //games.Add(game);                   if creating list of games
+            Game game = new TwentyOneGame();
+            game.Players = new List<Player>();
+            Player player = new Player();
+            player.Name = "Jesse";
+            game += player;
+            game -= player;
 
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "Jessie", "Bob", "Bill" };
-            game.ListPlayers();
+
+            Deck deck = new Deck();
+            deck.Shuffle(5);
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
             Console.ReadLine();
 
-
-            //Deck deck = new Deck();
-            //deck.Shuffle(5);
-
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.ReadLine();
-
         }
-
-
-
-        //public static Deck Shuffle(Deck deck, int times)
-        //{
-        //    for (int i = 0; i < times; i++)
-        //    {
-        //        deck = Shuffle(deck);
-        //    }
-        //    return deck;
-        //}
     }
 }
