81d0f8663273425e799474d6addad9fbf98b1f0f
Ramunas Siupinys <ramsiu@icloud.com>
2025-03-05 21:07:37 +0000

    Parameters Assignment
    Parameters Assignment

---
+++ Basic_C#_Programs/C# .NETFrameP2/ParametersAssignment/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    // Define a generic Employee class that takes a type parameter T.
+    public class Employee<T>
+    {
+        // Declare a property "Things" that is a list of type T.
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Basic_C#_Programs/C# .NETFrameP2/ParametersAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate an Employee object with type "string" as its generic parameter.
+            Employee<string> stringEmployee = new Employee<string>();
+            // Assign a list of strings to the "Things" property.
+            stringEmployee.Things = new List<string> { "Book", "Pen", "Laptop" };
+
+            // Instantiate an Employee object with type "int" as its generic parameter.
+            Employee<int> intEmployee = new Employee<int>();
+            // Assign a list of integers to the "Things" property.
+            intEmployee.Things = new List<int> { 1, 2, 3 };
+
+            // Create a loop to print all items in the stringEmployee's Things list.
+            Console.WriteLine("String: Employee Things:");
+            foreach (string item in stringEmployee.Things)
+            {
+                Console.WriteLine(item);  // Print each string item to the Console.
+            }
+
+            // Create a loop to print all items in the intEmployee's Things list.
+            Console.WriteLine("\nInt Employee Things:");
+            foreach (int number in intEmployee.Things)
+            {
+                Console.WriteLine(number); // Print each integer number to the Console.
+            }
+
+            // Wait for the user to press Enter before closing the console window.
+            Console.ReadLine();
+        }
+    }
+}
