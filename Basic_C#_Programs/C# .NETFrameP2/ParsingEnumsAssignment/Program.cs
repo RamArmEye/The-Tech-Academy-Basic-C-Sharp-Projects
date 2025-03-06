c6e71890982242dcb5093c6b162dc97ee6a4612a
Ramunas Siupinys <ramsiu@icloud.com>
2025-03-06 03:44:12 +0000

    Parsing Enums Assignment
    Parsing Enums Assignment, done

---
+++ Basic_C#_Programs/C# .NETFrameP2/ParsingEnumsAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParsingEnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter the current day of the week.
+            Console.Write("Please enter the current day of the week: ");
+            string userInput = Console.ReadLine();
+
+            try
+            {
+                // Attempt to parse the user input into the DaysOfWeek enum.
+                // The 'true' parameter makes the parsing case-insensitive.
+                DaysOfWeek currentDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), userInput, true);
+
+                // Display the parsed day.
+                Console.WriteLine("You entered: " + currentDay);
+            }
+            catch (Exception)
+            {
+                // If an error occurs during parsing, inform the user with an error message.
+                Console.WriteLine("Please enter an actual day of the week.");
+            }
+
+            // Wait for the user to press Enter before closing the console window.
+            Console.ReadLine();
+        }
+    }
+}
