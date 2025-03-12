97796244d2d9506b41a33a5dc56d060608a2a30c
Ramunas Siupinys <ramsiu@icloud.com>
2025-03-12 07:34:28 +0000

    Struct Assignment
    Struct Assignment

---
+++ Basic_C#_Programs/C# .NETFrameP2/Struct Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct_Assignment
+{
+    // Create a struct called Number.
+    public struct Number
+    {
+        // Define a property "Amount" of type decimal.
+        public decimal Amount { get; set; }
+    }
+
+    // The Program class contains the Main() method, the entry point of the application.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of the Number struct.
+            Number myNumber = new Number();
+
+            // Assign a value to the Amount property.
+            myNumber.Amount = 123.45m; // 'm' denotes that the value is a decimal literal.
+
+            // Print the value of Amount to the console.
+            Console.WriteLine("The amount is: " + myNumber.Amount);
+
+            // Wait for the user to press Enter before closing the console window.
+            Console.ReadLine();
+        }
+    }
+}
