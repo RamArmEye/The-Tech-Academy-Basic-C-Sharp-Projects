8b3e602c416c36a2f30e1a86c8d08623a68812df
Ramunas Siupinys <ramsiu@icloud.com>
2025-03-12 16:34:26 +0000

    Tutorial - Putting It All Together 1-5
    Tutorial - Putting It All Together Part 1-5 done

---
+++ Basic_C#_Programs/C# .NETFrameP2/Twenty One/TwentyOneDealer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Twenty_One
+{
+    public class TwentyOneDealer : Dealer
+    {
+        public List<Card> Hand { get; set; }
+        public bool Stay { get; set; }
+        public bool IsBusted { get; set; }
+
+        public static implicit operator string(TwentyOneDealer v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
