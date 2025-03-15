cf7b55aecdb22bb54ce63ebcae1d9bd1b880957d
Ramunas Siupinys <ramsiu@icloud.com>
2025-03-15 23:43:12 +0000

    Exception Handling Part 2
    Tutorial - More on Exception Handling Part 2 --all completed

--- Basic_C#_Programs/C# .NETFrameP2/Casino/TwentyOneGame.cs
+++ Basic_C#_Programs/C# .NETFrameP2/Casino/TwentyOneGame.cs
@@ -24,9 +24,21 @@ namespace Casino.TwentyOne
             Dealer.Deck = new Deck();
             Dealer.Deck.Shuffle();
             Console.WriteLine("Place your bet!");
+
             foreach (Player player in Players)
             {
-                int bet = Convert.ToInt32(Console.ReadLine());
+                bool validAnswer = false;
+                int bet = 0;
+                while (!validAnswer)
+                {
+                    Console.WriteLine("How much would you like to bet?");
+                    validAnswer = int.TryParse(Console.ReadLine(), out bet);
+                    if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+                }
+                if (bet < 0)
+                {
+                    throw new FraudException();
+                }
                 bool successfullyBet = player.Bet(bet);
                 if (!successfullyBet)
                 {
